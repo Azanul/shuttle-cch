8639cd88f3aeed68754ac9750c7e8498dd5beb9f
Azanul <azanulhaque@gmail.com>
2023-12-02 06:23:07 +0530

    feat: D1 Packet exclusive-cube not found
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web::ServiceConfig,  HttpResponse};
+use actix_web::{get, web, web::ServiceConfig,  HttpResponse};
 
 use shuttle_actix_web::ShuttleActixWeb;
 
@@ -19,4 +19,13 @@ async fn index() -> HttpResponse {
 #[get("/-1/error")]
 async fn error() -> HttpResponse {
     HttpResponse::InternalServerError().finish()
+}
+
+#[get("/1/{num1}/{num2220}")]
+async fn cubebits(path: web::Path<(u32, String)>) -> HttpResponse {
+    let (num1, num2220) = path.into_inner();
+    let num2 = num2220.split('/')
+                            .map(|x|x.parse::<u32>().unwrap())
+                            .fold(0, |xor, x| xor ^ x);
+    HttpResponse::Ok().body((num1 ^ num2).pow(3).to_string())
 }
