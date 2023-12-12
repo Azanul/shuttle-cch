2726bf981ab0a2f6aa72a18fb6ee522cfb3af2ba
Azanul <azanulhaque@gmail.com>
2023-12-12 15:11:54 +0530

    fix: D6 T2
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- core/src/day6.rs
+++ core/src/day6.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
 
+use fancy_regex::Regex;
+
 pub fn elf_counter(input_str: String) -> HashMap<&'static str, usize> {
-    let n_elves_on_shelves = input_str.matches("elf").count();
+    let n_elves_on_shelves = Regex::new("(?=(elf on a shelf))").unwrap().find_iter(&input_str).count();
     HashMap::from([
         ("elf", input_str.matches("elf").count()),
         ("elf on a shelf", n_elves_on_shelves),
-        ("shelf with no elf on it", input_str.matches("shelf").count() - n_elves_on_shelves)
+        ("shelf with no elf on it", input_str.matches("shelf").count().saturating_sub(n_elves_on_shelves))
     ])
 }
